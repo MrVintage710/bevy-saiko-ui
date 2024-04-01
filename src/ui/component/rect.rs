a4d50c055fa6f3427ed176a0d40e2815dcc06365
Brooks Palin <brookspalin710@gmail.com>
2024-04-01 19:25:45 +0900

    about to try and add text render, might touchup the renderer first

--- src/ui/component/rect.rs
+++ src/ui/component/rect.rs
@@ -18,6 +18,8 @@ pub struct RectComponent {
     size: Vec2,
     border_radius: Vec4,
     border_thickness: f32,
+    background_color: Color,
+    border_color: Color,
 }
 
 impl Default for RectComponent {
@@ -26,6 +28,8 @@ impl Default for RectComponent {
             size: Vec2::new(100.0, 100.0),
             border_radius: Vec4::new(10.0, 10.0, 10.0, 10.0),
             border_thickness: 5.0,
+            background_color: Color::rgba_u8(31, 33, 49, 255),
+            border_color: Color::rgba_u8(246, 92, 57, 255),
         }
     }
 }
@@ -34,9 +38,11 @@ impl SaikoComponent for RectComponent {
     fn render(&self, buffer: &mut SaikoBuffer) {
         buffer.push_rect(
             RectBuffer::default()
-                .with_position(Vec2::new(0.0, 0.0))
                 .with_size(self.size)
-                .with_color(Color::MAROON)
+                .with_color(self.background_color)
+                .with_border_radius(self.border_radius)
+                .with_border_width(self.border_thickness)
+                .with_border_color(self.border_color)
         );
     }
 }
